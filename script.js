86460240001a161e748496329645b63d876feae1
Linda Plasencia <lindaluz25@outlook.es>
2024-12-17 17:05:00 -0500

    arreglo day3-1

--- script.js
+++ script.js
@@ -16,22 +16,20 @@ setInterval(createHeart, 300);
 
 
 
-/*audio*/ 
+
 const audioElement = document.getElementById("audio1");
 const btnAudioControl = document.getElementById("btnAudioControl");
 
-// Agregamos el evento click al botón
+
 btnAudioControl.addEventListener("click", () => {
   if (audioElement.muted) {
-    // Si está silenciado, activamos el sonido
     audioElement.muted = false;
-    btnAudioControl.innerText = "🔊"; // Cambiamos el ícono
+    btnAudioControl.innerText = "🔊"; 
     audioElement.play();
-    audioElement.volume = 0.5; // Volumen inicial
+    audioElement.volume = 0.5; 
   } else {
-    // Si no está silenciado, lo silenciamos
     audioElement.muted = true;
-    btnAudioControl.innerText = "🔇"; // Cambiamos el ícono
+    btnAudioControl.innerText = "🔇"; 
   }
 });
 
@@ -41,7 +39,7 @@ btnAudioControl.addEventListener("click", () => {
 const hamburger = document.getElementById('hamburger');
 const navbar = document.getElementById('navbar');
 
-// Evento de clic en el botón hamburguesa
+
 hamburger.addEventListener('click', () => {
-  navbar.classList.toggle('show'); // Alternar la clase 'show'
+  navbar.classList.toggle('show'); 
 });
